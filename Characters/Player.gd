43795c762181fddb68a303181a4014fb8b42183a
Stonewms0404 <48721922+Stonewms0404@users.noreply.github.com>
2024-02-21 21:58:14 -0500

    StateMachine + Animations + Pausing

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -3,29 +3,42 @@ extends CharacterBody2D
 class_name Player
 
 @export var speed : float
+@export var friction : float
+
+@onready var anim : AnimationTree = $AnimationTree
+
 var movement : Vector2
-@export var friction: float
 var facing : Vector2
 
 func _physics_process(_delta):
+	movement = Input.get_vector("left", "right", "up", "down")
+	
 	if (Input.is_action_pressed("left")):
 		facing.x = -1
-		movement.x = speed * facing.x
 	elif (Input.is_action_pressed("right")):
 		facing.x = 1
-		movement.x = speed * facing.x
-	else:
-		movement.x = lerp(movement.x, 0.0, friction)
 	
 	if (Input.is_action_pressed("up")):
 		facing.y = -1
-		movement.y = speed * facing.y
 	elif (Input.is_action_pressed("down")):
 		facing.y = 1
-		movement.y = speed * facing.y
-	else:
-		movement.y = lerp(movement.y, 0.0, friction)
 	
-	velocity = movement
+	velocity = lerp(movement * speed, Vector2.ZERO, friction)
+	
+	if abs(velocity.x) < .5 && abs(velocity.y) < .5:
+		velocity = Vector2.ZERO
+	
+	change_animation()
 	
 	move_and_slide()
+
+func change_animation():
+	if velocity == Vector2.ZERO:
+		anim["parameters/conditions/idle"] = true
+		anim["parameters/conditions/is_moving"] = false
+	else:
+		anim["parameters/conditions/idle"] = false
+		anim["parameters/conditions/is_moving"] = true
+	
+	anim["parameters/Idle/blend_position"] = movement;
+	anim["parameters/Walk/blend_position"] = movement;
